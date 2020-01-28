219b987efdb8ac9de73f1e2138461d0bf62116b3
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-28 17:15:50 +0300

    EndpointRoutingMiddleware

--- Routing/Routing/Startup.cs
+++ Routing/Routing/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -12,13 +13,44 @@ namespace Routing
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseRouting();
+            app.Use(async (context, next) =>
+            {
+                Endpoint endpoint = context.GetEndpoint();
+
+                if (endpoint != null)
+                {
+                    var routePattern = (endpoint as Microsoft.AspNetCore.Routing.RouteEndpoint)?.RoutePattern?.RawText;
+
+                    Debug.WriteLine($"Endpoint Name: {endpoint.DisplayName}");
+                    Debug.WriteLine($"Route Pattern: {routePattern}");
+
+                    await next();
+                }
+                else
+                {
+                    Debug.WriteLine("Endpoint: null");
+                    await context.Response.WriteAsync("Endpoint is not defined");
+                }
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/index", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Index!");
+                });
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
         }
     }
 }
