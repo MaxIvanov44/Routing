6db951c00d7c058c228695d2f63c8bef51d55b63
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-28 17:27:20 +0300

    RouterMiddleware

--- Routing/Routing/Startup.cs
+++ Routing/Routing/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,44 +14,63 @@ namespace Routing
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            var myRouteHandler = new RouteHandler(Handle);
+            var routeBuilder = new RouteBuilder(app, myRouteHandler);
+            routeBuilder.MapRoute("default", "{controller}/{action}");
+            app.UseRouter(routeBuilder.Build());
+            app.Run(async (context) =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-            app.Use(async (context, next) =>
-            {
-                Endpoint endpoint = context.GetEndpoint();
-
-                if (endpoint != null)
-                {
-                    var routePattern = (endpoint as Microsoft.AspNetCore.Routing.RouteEndpoint)?.RoutePattern?.RawText;
-
-                    Debug.WriteLine($"Endpoint Name: {endpoint.DisplayName}");
-                    Debug.WriteLine($"Route Pattern: {routePattern}");
-
-                    await next();
-                }
-                else
-                {
-                    Debug.WriteLine("Endpoint: null");
-                    await context.Response.WriteAsync("Endpoint is not defined");
-                }
-            });
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/index", async context =>
-                {
-                    await context.Response.WriteAsync("Hello Index!");
-                });
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                await context.Response.WriteAsync("Hello World!");
             });
         }
+        private async Task Handle(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello ASP.NET Core!");
+        }
     }
+    //public class Startup
+    //{
+    //    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    //    {
+    //        if (env.IsDevelopment())
+    //        {
+    //            app.UseDeveloperExceptionPage();
+    //        }
+
+    //        app.UseRouting();
+    //        app.Use(async (context, next) =>
+    //        {
+    //            Endpoint endpoint = context.GetEndpoint();
+
+    //            if (endpoint != null)
+    //            {
+    //                var routePattern = (endpoint as Microsoft.AspNetCore.Routing.RouteEndpoint)?.RoutePattern?.RawText;
+
+    //                Debug.WriteLine($"Endpoint Name: {endpoint.DisplayName}");
+    //                Debug.WriteLine($"Route Pattern: {routePattern}");
+
+    //                await next();
+    //            }
+    //            else
+    //            {
+    //                Debug.WriteLine("Endpoint: null");
+    //                await context.Response.WriteAsync("Endpoint is not defined");
+    //            }
+    //        });
+    //        app.UseEndpoints(endpoints =>
+    //        {
+    //            endpoints.MapGet("/index", async context =>
+    //            {
+    //                await context.Response.WriteAsync("Hello Index!");
+    //            });
+    //            endpoints.MapGet("/", async context =>
+    //            {
+    //                await context.Response.WriteAsync("Hello World!");
+    //            });
+    //        });
+    //    }
+    //}
+
 }
